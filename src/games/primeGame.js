2b16e748310bbb3f0ba098adb7bd956b96dd81bc
Dmitrii <heybehonestproduction@gmail.com>
2023-03-13 00:01:33 +0100

    fix for git status git status

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -5,12 +5,14 @@ import startGame from '../index.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getPrime = (num) => {
-  const limit = Math.sqrt(num);
-  for (let i = 2; i <= limit; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
+  if (num < 2 || (num > 2 && num % 2 === 0)) return false;
+
+  const maxDivisor = Math.floor(Math.sqrt(num)) + 1;
+
+  for (let i = 3; i < maxDivisor; i += 2) {
+    if (num % i === 0) return false;
   }
+
   return true;
 };
 
