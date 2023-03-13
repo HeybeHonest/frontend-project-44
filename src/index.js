2c11d4fe819deeb340ca5a20ff166f5f22ba8462
Dmitrii <heybehonestproduction@gmail.com>
2023-03-13 12:39:13 +0100

    try to beat pass hexlet checking #1

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const startGame = (gameTitle, gameEngine) => {
 
     if (answer !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}.`);
       return;
     }
     console.log('Correct!');
